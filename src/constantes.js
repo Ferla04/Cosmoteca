18325e3b3052a96c810513c618d83bf86423b41e
Ferla04 <fer.velas135@gmail.com>
2023-05-05 16:43:45 -0500

    Fetching de datos

---
+++ src/constantes.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'AIzaSyAsqEP_p2u8jFg3gikFk9iEI6q01RUyHJ0'
+export const NO_IMAGE = 'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg'
---
+++ src/services/books.js
@@ -0,0 +1,22 @@
+import { API_KEY, NO_IMAGE } from '../constantes'
+
+export const searchBooks = async ({ search }) => {
+  if (search === '') return null
+
+  try {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?key=${API_KEY}&q=${search}&maxResults=40`)
+    const data = await response.json()
+    const books = data.items
+
+    return books?.map(({ id, volumeInfo }) => ({
+      id,
+      authors: volumeInfo.authors,
+      title: volumeInfo.title,
+      image: volumeInfo.imageLinks?.thumbnail ?? NO_IMAGE,
+      year: volumeInfo.publishedDate
+    }))
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error searching movies')
+  }
+}
